9530e01b3c99adaeec4f5788d02dfa73cd8cab34
useramc <chinmayiamc@gmail.com>
2026-06-08 09:55:55 +0530

    Add incident_history_tool

---
+++ tools/incident_history_tool.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+def incident_history_tool(ip_address):
+    df_auth=pd.read_csv('data/cleaned_logs/auth_logs_cleaned.csv')
+    df_port=pd.read_csv('data/cleaned_logs/port_scan_logs_cleaned.csv')
+
+    ip_address = str(ip_address).strip()
+
+    auth_records = df_auth[df_auth["source_ip"] == ip_address]
+    port_records = df_port[df_port["source_ip"] == ip_address]
+    
+    if auth_records.empty and port_records.empty:
+        return {"ip_address": ip_address,
+                "total_events": 0,
+                "found": False }
+    else:
+        total_events =len(auth_records)+len(port_records)
+        failed_auth_logs = (auth_records["event_type"] == "LOGIN_FAILED").sum()
+        successful_auth_logs = (auth_records["event_type"] == "LOGIN_SUCCESS").sum()
+        #print(failed_auth_logs)
+        selected_auth_events = ["timestamp","event_type","username","is_admin"]
+        auth_events=auth_records[selected_auth_events].to_dict(orient='records')
+
+        selected_port_events=["timestamp","destination_port","action","connection_state"]
+        port_events=port_records[selected_port_events].to_dict(orient='records')
+
+        
+        countries = list(set(auth_records["country"])|set(port_records["country"]))
+        unique_ports_scanned=port_records["destination_port"].nunique()
+
+        auth_times=pd.to_datetime(auth_records["timestamp"])
+        port_times=pd.to_datetime(auth_records["timestamp"])
+        all_times=pd.concat([auth_times,port_times])
+        first_seen=all_times.min()
+        last_seen=all_times.max()
+
+        
+
+        return {
+            "ip_address": ip_address,
+            "total_events": total_events,
+            "auth_events": auth_events,
+            "port_scan_events":port_events,
+            "failed_logins": int(failed_auth_logs),
+            "successful_logins": int(successful_auth_logs),
+            "unique_ports_scanned":unique_ports_scanned,
+            "countries":countries,
+            "first_seen":first_seen,
+            "last_seen":last_seen,
+            "found":True
+        }
+        return 0
+
+
+result = incident_history_tool('194.165.16.72')
+print(result)
